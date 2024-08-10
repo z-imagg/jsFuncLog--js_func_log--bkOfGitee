362ea3ddd196788c4d58ce2df99950c3f77fe128
prgrmz07 <prgrmz07@163.com>
2024-08-10 17:45:06 +0800

    fix5

--- log_recv_web_srv/main.py
+++ log_recv_web_srv/main.py
@@ -6,6 +6,7 @@ from flask import Flask, request##, jsonify, abort
 from io import TextIOWrapper
 import threading
 import traceback
+import typing
 
 app = Flask(__name__)
 
@@ -13,18 +14,19 @@ app = Flask(__name__)
 def hello():
     return "hello!!"
 
-g_logFile:TextIOWrapper=open("_func_log.txt","+a")
+g_logFile:typing.Union[TextIOWrapper|None]=None
 g_threaLock=threading.Lock()
+
 
 def write_file__with_ThreadLock( text:str):
   global g_threaLock,g_logFile
-  assert g_logFile is not None, f"文件已关闭?,g_logFile={g_logFile}"
   #加锁
   getLockOk:bool=g_threaLock.acquire(blocking=True,timeout=2)
   if not getLockOk:
     raise Exception(f"write_file__with_ThreadLock,获取线程锁失败, logFilePath={g_logFile}")
   
   #业务开始 (有锁确保串行)
+  _openFile_ifNone__noThreadLock()
   try:
     g_logFile.write(text)
   #业务结束 (有锁确保串行)
@@ -37,6 +39,10 @@ def write_file__with_ThreadLock( text:str):
   g_threaLock.release()
   #end_func write_file__with_ThreadLock
 
+def _openFile_ifNone__noThreadLock():
+  if g_logFile is None: 
+    g_logFile=open("_func_log.txt","+a")
+
 def close_file__with_ThreadLock( ):
   global g_threaLock,g_logFile
   #加锁
@@ -44,6 +50,8 @@ def close_file__with_ThreadLock( ):
   if not getLockOk:
     raise Exception(f"close_file__with_ThreadLock, 获取线程锁失败, logFilePath={g_logFile}")
   
+  if g_logFile is None: return
+
   #业务开始 (有锁确保串行)
   try:
     g_logFile.flush()
@@ -71,7 +79,6 @@ def _writeLine_funcLogFile():
   # if not request.json or not request.is_json:
   #     return "err__notJson",402
 
-  global g_logFile
   req_body:bytes=request.data
   # msg:str=f"req.body=#{req_body}#,req.content_type=#{request.content_type}#"
   # print(msg)
@@ -92,3 +99,4 @@ def _close_funcLogFile():
 if __name__ == '__main__':
     # 指定端口为5001
     app.run(debug=True, port=5001)
+    close_file__with_ThreadLock()
