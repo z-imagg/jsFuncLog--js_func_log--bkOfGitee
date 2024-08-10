c3bb76a1fedaf376a4aa88a7f55b36b0205789cc
prgrmz07 <prgrmz07@163.com>
2024-08-10 19:00:52 +0800

    fix4

--- src/_debug_log/IndexedDB_wrap.js
+++ src/_debug_log/IndexedDB_wrap.js
@@ -1,19 +1,25 @@
 // 运行时动态加载 Dexie.js(IndexedDB包裹)， 以避开将dexie.js打包进此项目js编译产物(从而避开es版本差异等各种坑)
 // https://github.com/dexie/Dexie.js
 
-export function loadDexie() {
-  return new Promise((resolve, reject) => {
+export function loadDexie_once() {
+  return new Promise((resolve_busz, reject) => {
+    //若已加载 dexie.js, 则 执行业务 后 直接返回
+    if(window.DexieJs_loaded){
+      resolve_busz();
+      return;
+    }
     const script = document.createElement('script');
     script.src = 'https://unpkg.com/dexie/dist/dexie.js';
     script.onload = () => {
       console.log('Dexie loaded successfully');
-      resolve();
+      resolve_busz();
     };
     script.onerror = () => {
       console.error('Failed to load Dexie');
       reject(new Error('Script load failed'));
     };
     document.head.appendChild(script);
+    window.DexieJs_loaded=true;//已加载dexie.js标记
   });
 }
 
@@ -28,7 +34,7 @@ function createDb_IndexedDB(){
 export function writeFuncLog_IndexedDB(direction/* :string */, srcFile/* :string */, method/* :string */){
 
 
-  loadDexie().then(() => {
+  loadDexie_once().then(() => {
     
     createDb_IndexedDB()
 
