f6c66720add7fb8964bf6f20018b12418a1992ed
prgrmz07 <prgrmz07@163.com>
2024-08-11 10:23:30 +0800

    以 1/relative_us_modNum 的概率,执行此函数的业务(writeFuncLog_IndexedDB)

--- src/_debug_log/IndexedDB_wrap.js
+++ src/_debug_log/IndexedDB_wrap.js
@@ -2,20 +2,20 @@
 // https://github.com/dexie/Dexie.js
 
 //构造概率 1/abs_ms_modNum 
-function probability__eq__0_div__abs_ms_modNum(cur_ms,abs_ms_modNum/* :int */)/* :boolean */{
-  const yes/* :boolean */= cur_ms%abs_ms_modNum==0;
+function probability__eq__0_div__relative_us_modNum(relative_us,us_modNum/* :int */)/* :boolean */{
+  const yes/* :boolean */= relative_us%us_modNum==0;
   return yes;
 }
 export function writeFuncLog_IndexedDB(direction/* :string */, srcFile/* :string */, method/* :string */){
-  const cur_ms=new Date().valueOf()
-
-  // 以 1/abs_ms_modNum 的概率,执行此函数的业务(writeFuncLog_IndexedDB)
-  if(probability__eq__0_div__abs_ms_modNum(cur_ms,window._debugVar.abs_ms_modNum?1000:window._debugVar.abs_ms_modNum)){
-
+  const relative_us=window.performance.now() 
+  // 以 1/relative_us_modNum 的概率,执行此函数的业务(writeFuncLog_IndexedDB)
+  if(probability__eq__0_div__relative_us_modNum(relative_us,window._debugVar.relative_us_modNum?1000:window._debugVar.relative_us_modNum)){
+    
     const tab_funcLog=window.db_FuncLog.tab_funcLog;
-
+    
     const debugDiv = document.getElementById("id_debugDiv")
     if(debugDiv){
+      const cur_ms=new Date().valueOf()
       const delta_ms=cur_ms-window._debugVar.start_ms
       const delta_s=parseInt(delta_ms/1000)
       const rowCnt_w=parseInt(window._debugVar._rowCnt/10000)
